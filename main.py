00570b1bd45926047d9a241516a6093fad7db1e7
Emmanuel Ojo <ojoemmaue058@gmail.com>
2025-07-18 03:03:19 +0100

    Refactored data processing by converting Series to DataFrame using reset_index()

--- main.py
+++ main.py
@@ -7,19 +7,12 @@ squirrel_data = pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Da
 squirrel_fur_colours_count = squirrel_data["Primary Fur Color"].value_counts()
 # print(squirrel_fur_colours_count)
 
-available_squirrel_colours = []
-squirrel_fur_colours_count_list = []
-for color, count in squirrel_fur_colours_count.items():
-    available_squirrel_colours.append(color)
-    squirrel_fur_colours_count_list.append(count)
+# Convert to DataFrame
+squirrel_fur_colours_count_dataframe = squirrel_fur_colours_count.reset_index()
+squirrel_fur_colours_count_dataframe.columns = ["Fur Colour", "Count"]
 
-# creating a dictionary from the "squirrel_fur_colours_count" series
-squirrel_fur_colours_count_dict = {
-    "Fur Colour": available_squirrel_colours,
-    "Count": squirrel_fur_colours_count_list
-}
 
-# convert the dictionary to a dataframe and create the csv file
-squirrel_fur_colours_count_dataframe = pandas.DataFrame(squirrel_fur_colours_count_dict)
-squirrel_fur_colours_count_dataframe.to_csv("squirrel.count.csv")
+# Export to CSV
+squirrel_fur_colours_count_dataframe.to_csv("squirrel.count.csv", index=False)
+
 
